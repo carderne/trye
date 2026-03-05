b3f7248f83399058289955bf0068419ee15a904f
Chris Arderne <chris@rdrn.me>
2026-03-05 14:28:06 +0000

    add ci

--- src/trye/__init__.py
+++ src/trye/__init__.py
@@ -5,7 +5,6 @@ Wraps callable invocations in a Result type, capturing exceptions
 as Err values instead of raising them.
 """
 
-__version__ = "0.1.0"
 __all__ = ["Ok", "Err", "Result", "trye"]
 
 from .trye import Err, Ok, Result, trye
